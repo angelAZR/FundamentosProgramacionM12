44dc2894e477a6346bebaa397850bc48cef7daad
angelAZR <angel.alvarez@upb.edu.co>
2026-02-20 12:38:14 -0500

    Condicionales simples
    Condicionales simples

---
+++ 2.CondicionalesSimples/2.CondicionalesSimples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*           String nombre = "";
+              float sueldo = 0;
+              Console.WriteLine("Ingresar el nombre del usuario");
+              nombre = Console.ReadLine();//Lo que llega de la consola es de tipo string
+                        Console.WriteLine("ingrese el sueldo del usuario");
+                        sueldo = Convert.ToSingle (Console.ReadLine());
+                        if (sueldo>=3000)
+                        {
+                            Console.WriteLine($"lapersona {nombre}, debe abonar impuestos");
+                        }*/
+            string nombre = "";
+            int edad = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese edad del usuario");
+            edad = Convert.ToInt32 (Console.ReadLine());
+            if (edad >= 18) 
+            {
+                Console.WriteLine("Bienvenido a mi sito web");
+            }
+
+
+        }
+    }
+}
