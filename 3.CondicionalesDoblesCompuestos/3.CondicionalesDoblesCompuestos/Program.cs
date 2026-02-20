331a1f5cf5a1fd3eb8f9db05768e02e90ca03058
angelAZR <angel.alvarez@upb.edu.co>
2026-02-20 13:32:37 -0500

    Condicionales Dobles Compuestos
    Condicionales y tarea hacer de chill

---
+++ 3.CondicionalesDoblesCompuestos/3.CondicionalesDoblesCompuestos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _3.CondicionalesDoblesCompuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   string nombre = "";
+               float sueldo = 0;
+               Console.WriteLine("Ingrse el nombre del usuario");
+               nombre=Console.ReadLine();
+               Console.WriteLine("Ingrese el sueldo del usuario");
+               sueldo= Single.Parse(Console.ReadLine() );
+
+               if (sueldo>=3000)
+               {
+                   Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+               }
+               else
+               {
+                   Console.WriteLine("la persona esta exenta de impuestos");
+               }*/
+
+            float numero1 = 0;
+            float numero2 = 0;
+            Console.WriteLine("Ingresar numero 1");
+            numero1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar numero 2");
+            numero2 = Single.Parse(Console.ReadLine());
+            if (numero1 > numero2)
+            {
+
+            }
+
+        }
+    }
+}
