27862eaf894ec2a2c70d5dbc2004a8ffb7966f38
angelAZR <angel.alvarez@upb.edu.co>
2026-02-21 17:18:54 -0500

    Add files via upload
    raar

---
+++ 4.EjercicioProgramacion/4.EjercicioProgramacion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4.EjercicioProgramacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          int numEnt = 0;
+          Console.WriteLine("Ingrese el numero");
+          numEnt = Convert.ToInt32(Console.ReadLine());
+            if (numEnt < 0)
+            {
+                Console.WriteLine($"El numero {numEnt} es negativo");
+            }
+            else 
+            { 
+                Console.WriteLine($"El numer {numEnt} es positivo"); 
+            }
+                     
+        }
+    }
+}
---
+++ 4.EjercicioProgramacion/4.EjercicioProgramacion/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("4.EjercicioProgramacion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4.EjercicioProgramacion")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("fbda0cd9-edb7-4238-98d6-a1ba965ad2fe")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
