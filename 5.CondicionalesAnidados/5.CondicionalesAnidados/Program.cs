283b6557b5cce28dc5b5237c199f4de59c0829d9
angelAZR <angel.alvarez@upb.edu.co>
2026-02-25 13:23:46 -0500

    Add files via upload

---
+++ 5.CondicionalesAnidados/5.CondicionalesAnidados/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _5.CondicionalesAnidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            Console.WriteLine("Ingrese 3 valores diferentes");
+            Console.WriteLine("Ingrese el valor del numero 1");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del numero 2");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del numero 3");
+            num3 = Int32.Parse(Console.ReadLine());
+            if (num1 > num2) 
+            {
+                if (num1 > num3) 
+                {
+                    Console.WriteLine($"El numero mayor es; {num1}");
+
+                }
+                else 
+                {
+                    Console.WriteLine($"El numero mayor es: {num3}");
+                }
+            }
+            else 
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine($"El numero mayor es; {num2}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"El numero mayor es: {num3}");
+                }
+            }
+        }
+    }
+}
