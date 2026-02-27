d09bc1908d434bb6db6e34902729719869373ac6
angelAZR <angel.alvarez@upb.edu.co>
2026-02-27 13:31:58 -0500

    Add files via upload

---
+++ 6.CondicionalAnidado2/6.CondicionalAnidado2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.CondicionalAnidado2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1 = 0;
+            float nota2 = 0;
+            float nota3 = 0;
+            float promedio = 0;
+            Console.WriteLine("Ingrese la nota 1");
+            nota1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la noya 2");
+            nota2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota 3");
+            nota3 = Single.Parse(Console.ReadLine());
+            promedio= (nota1 + nota2 +  nota3) / 3;
+            if (promedio >= 9.5 && promedio <= 10.0)
+            {
+                Console.WriteLine("Excelente");
+            }
+            else 
+            {
+                if(promedio >= 8.5 && promedio<= 9.4) 
+                {
+                    Console.WriteLine("Muy bien");
+                }
+                else 
+                {
+                    if (promedio >= 7.5 && promedio < 8.5)
+                    {
+                        Console.WriteLine("Bien");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Promedio erroneo");
+                    }
+                }
+            
+            }
+            
+        }
+    }
+}
---
+++ 7.CondicionalesMultiplesOperadorTernario/7.CondicionalesMultiplesOperadorTernario/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace _7.CondicionalesMultiplesOperadorTernario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //string diaSemana = "";
+             //float subtotal = 0;
+             //float descuento = 0;
+             //Console.WriteLine("Ingrese monto de la compra");
+             //subtotal = Single.Parse(Console.ReadLine());
+             //Console.WriteLine("Ingrese el dia de la semana");
+             //diaSemana=Console.ReadLine();
+
+             //switch (diaSemana) 
+             //{
+             //    case "Lunes":
+             //        descuento=subtotal*0.1f;
+             //        Console.WriteLine($"Hoy es lunes, el descuento es de {descuento}, el valor total a pagar es de {subtotal-descuento}");
+             //        break;
+             //    case "Martes":
+             //        descuento = subtotal * 0.15f;
+             //        Console.WriteLine($"Hoy es martes, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+             //        break;
+             //    case "Miercoles":
+             //        descuento = subtotal * 0.10f;
+             //        Console.WriteLine($"Hoy es miercoles, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+             //        break;
+             //    case "Jueves":
+             //        descuento = subtotal * 0.15f;
+             //        Console.WriteLine($"Hoy es jueves, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+             //        break;
+             //    case "Viernes":
+             //        descuento = subtotal * 0.2f;
+             //        Console.WriteLine($"Hoy es viernes, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+             //        break;
+             //    case "Sabado":
+             //        descuento = subtotal * 0.2f;
+             //        Console.WriteLine($"Hoy es sabado, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+             //        break;
+             //    case "Domingo":
+             //        descuento = 0;
+             //        Console.WriteLine($"Hoy es domingo, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+             //        break;
+
+             //    default:
+             //        Console.WriteLine("Por favor ingrese un dia de la semana correcto");
+             //        break;*/
+            int num1;
+            int num2;
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            int division = num1 / num2;
+            int numSRMD = 0;
+            Console.WriteLine("Ingrese numero 1");
+            num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese numero 2");
+            num2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Elija: Sumar = 1, restar = 2, multiplicar = 3 o dividir = 4 ");
+            numSRMD = Convert.ToInt32(Console.ReadLine());
+            if (numSRMD == 1) 
+            {
+                Console.WriteLine($"La suma de {num1} y {num2} es {suma}");
+            }
+
+        }
+    }
+    
+}
