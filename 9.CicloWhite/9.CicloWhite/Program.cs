54a988b7e119ddf196ce14e878d05160efefc850
angelAZR <angel.alvarez@upb.edu.co>
2026-03-11 13:29:46 -0500

    CicloWhite

---
+++ 9.CicloWhite/9.CicloWhite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.CicloWhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int contador = 1;
+              int acumulador = 0;
+              while (contador <= 5) 
+              {
+                  acumulador += contador;
+                  Console.WriteLine($"Contador:{contador} - Acumulador: {acumulador}");
+                  contador++;
+              }
+              Console.WriteLine($"La suma de los cinco primeros números enteros es:{acumulador}");*/
+
+            int num = 0;
+            int contador = 1;
+            int acumulador = num;
+            Console.WriteLine("Ingrese el numero a calcular");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            while (contador <= num) 
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador:{contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"La factorisacion de {num} es: {acumulador} ");
+        }
+    }
+}
