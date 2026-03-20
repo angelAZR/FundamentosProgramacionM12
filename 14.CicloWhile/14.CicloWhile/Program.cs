81f052f30e52dcfcd23c1bdf2310f93d0efad71c
angelAZR <angel.alvarez@upb.edu.co>
2026-03-20 13:28:58 -0500

    cicloWhile

---
+++ 14.CicloWhile/14.CicloWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar numeros: contar cuantos son pares y cuantos impares calcular el promedio de todos los numeros ingresados terminar cuando el usuario lo indique*/
+            int contadorNumeros = 1;
+            int contador = 1;
+
+
+        }
+    }
+}
