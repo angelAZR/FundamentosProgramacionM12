299dccbccdf95552aa59938a56bf0fd1f221f359
angelAZR <angel.alvarez@upb.edu.co>
2026-03-25 13:26:52 -0500

    15.CicloFord

---
+++ 15.CicloFor/15.CicloFor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _15.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+/*         int acumulador = 1;
+
+            for (int contador = 1; contador <=5; contador++) 
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine($"La suma de los cinco primeros numeros enteros es {acumulador}");*/
+
+          /*  int inicioRango;
+            int finalRango;
+            Console.WriteLine("Ingrese el inicio del rango");
+            inicioRango = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el final del rango");
+            finalRango = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"los numeros multiplos de 5 de {inicioRango} a {finalRango} son: ");
+            for (int contador = inicioRango; contador <= finalRango; contador++) 
+            {
+                if (contador % 5 == 0) 
+                {
+                    Console.WriteLine(contador);
+                }
+            }*/
+
+
+
+        }
+    }
+}
